d60265730a3f70e7c341f63ef8cea7d5e93e3e63
egenerse <egenerse@gmail.com>
2024-12-15 21:10:23 +0300

    chore: setup server package.json adn configuration

--- standalone/server/eslint.config.mjs
+++ standalone/server/eslint.config.mjs
@@ -1,12 +1,27 @@
-import rootConfig from "../../eslint.config.mjs";
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettierConfig from 'eslint-config-prettier';
 
 export default [
-  ...rootConfig,
   {
+    files: ['src/**/*.ts', 'src/**/*.tsx'],
+    ignores: ['node_modules', 'dist', 'vite.config.ts'],
     languageOptions: {
+      parser: tsParser,
       parserOptions: {
-        project: "./tsconfig.json", // Points to the backend tsconfig.json
+        project: ['./tsconfig.json'],
+        ecmaVersion: 'latest',
+        sourceType: 'module',
       },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...tseslint.configs.strict.rules,
+      ...tseslint.configs.stylistic.rules,
+      ...prettierConfig.rules,
     },
   },
 ];
