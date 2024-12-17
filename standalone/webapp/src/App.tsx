6f9ed5ab234d39d96804c8c3d91481e0e9bbdb10
egenerse <egenerse@gmail.com>
2024-12-17 14:11:59 +0300

    feat: run format:check script in pre commit stage

--- standalone/webapp/src/App.tsx
+++ standalone/webapp/src/App.tsx
@@ -1,14 +1,14 @@
-import { useState } from "react";
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
     <div>
       {count}
       <button onClick={() => setCount(count + 1)}>Increment</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
