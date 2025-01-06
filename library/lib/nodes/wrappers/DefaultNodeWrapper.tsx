7fc8086ce8ab6faff037468ff22a973e2adba4e8
egenerse <egenerse@gmail.com>
2025-01-06 23:02:20 +0100

    feat: add more handler to default node wrapper

--- library/lib/nodes/wrappers/DefaultNodeWrapper.tsx
+++ library/lib/nodes/wrappers/DefaultNodeWrapper.tsx
@@ -8,10 +8,58 @@ export function DefaultNodeWrapper({ children }: Props) {
   return (
     <>
       {children}
+      <Handle
+        id="top-left"
+        type="source"
+        position={Position.Top}
+        style={{ left: "calc(15%)" }}
+      />
       <Handle id="top" type="source" position={Position.Top} />
+      <Handle
+        id="top-right"
+        type="source"
+        position={Position.Top}
+        style={{ left: "calc(85%)" }}
+      />
+      <Handle
+        id="right-top"
+        type="source"
+        position={Position.Right}
+        style={{ top: "calc(15%)" }}
+      />
       <Handle id="right" type="source" position={Position.Right} />
+      <Handle
+        id="right-bottom"
+        type="source"
+        position={Position.Right}
+        style={{ top: "calc(85%)" }}
+      />
+      <Handle
+        id="bottom-right"
+        type="source"
+        position={Position.Bottom}
+        style={{ left: "calc(85%)" }}
+      />
       <Handle id="bottom" type="source" position={Position.Bottom} />
+      <Handle
+        id="bottom-left"
+        type="source"
+        position={Position.Bottom}
+        style={{ left: "calc(15%)" }}
+      />
+      <Handle
+        id="left-bottom"
+        type="source"
+        position={Position.Left}
+        style={{ top: "calc(85%)" }}
+      />
       <Handle id="left" type="source" position={Position.Left} />
+      <Handle
+        id="left-top"
+        type="source"
+        position={Position.Left}
+        style={{ top: "calc(15%)" }}
+      />
     </>
   )
 }
