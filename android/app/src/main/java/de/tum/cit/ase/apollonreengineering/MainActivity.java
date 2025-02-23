8090e44d33ed0f2745feeeb271eef500e746e7e1
Belemir Kürün <kurunbelemir@gmail.com>
2025-02-23 12:08:08 +0100

    feat: add capacitor

---
+++ android/app/src/main/java/de/tum/cit/ase/apollonreengineering/MainActivity.java
@@ -0,0 +1,5 @@
+package de.tum.cit.ase.apollonreengineering;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.tum.cit.ase.apollonreengineering',
+  appName: 'Apollon Reengineering Mobile',
+  webDir: 'standalone/webapp/dist'
+};
+
+export default config;
