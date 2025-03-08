8b8cc7ca52ad5f92bcae0589bd6281278245e441
egenerse <egenerse@gmail.com>
2025-03-08 15:08:00 +0100

    feat: install react-router and wrap application

--- standalone/webapp/src/App.tsx
+++ standalone/webapp/src/App.tsx
@@ -1,9 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router"
 import { LibraryView } from "./LibraryView"
 
 function App() {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
-      <LibraryView />
+      <BrowserRouter>
+        <Routes>
+          <Route path=":diagramId" element={<LibraryView />} />
+          <Route path="/" element={<LibraryView />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
