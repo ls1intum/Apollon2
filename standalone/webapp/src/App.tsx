a24f682afab8dcce540ab93bbc0f26189f40efd5
egenerse <egenerse@gmail.com>
2025-03-16 10:30:30 +0100

    feat: add safe area view to keep content in safeare in mobile

--- standalone/webapp/src/App.tsx
+++ standalone/webapp/src/App.tsx
@@ -3,6 +3,30 @@ import { AppProviders } from "./AppProviders"
 import { Navbar } from "./components"
 import "@xyflow/react/dist/style.css"
 import { Apollon, ApollonWithCollaboration, ErrorPage } from "@/pages"
+import { SafeArea } from "capacitor-plugin-safe-area"
+import "./webapp.css"
+
+// To set the safe area insets as for mobile devices
+SafeArea.getSafeAreaInsets().then(
+  ({ insets: { top, bottom, left, right } }) => {
+    document.documentElement.style.setProperty(
+      "--safe-area-inset-top",
+      `${top}px`
+    )
+    document.documentElement.style.setProperty(
+      "--safe-area-inset-bottom",
+      `${bottom}px`
+    )
+    document.documentElement.style.setProperty(
+      "--safe-area-inset-left",
+      `${left}px`
+    )
+    document.documentElement.style.setProperty(
+      "--safe-area-inset-right",
+      `${right}px`
+    )
+  }
+)
 
 function App() {
   return (
