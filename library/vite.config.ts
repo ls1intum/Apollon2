d97d556d93d9553814f5cf6c1722a905c4131b2a
Belemir Kürün <kurunbelemir@gmail.com>
2025-04-13 22:20:48 +0200

    ci: try new. package for css injection

--- library/vite.config.ts
+++ library/vite.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import dts from "vite-plugin-dts"
-import { resolve } from "path"
-import { libInjectCss } from "vite-plugin-lib-inject-css"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import { resolve } from "path";
+import postcss from "rollup-plugin-postcss";
 
 export default defineConfig({
-  plugins: [react(), dts({ include: ["lib"] }), libInjectCss()],
+  plugins: [
+    react(),
+    dts({ include: ["lib"] })
+    // Removed libInjectCss(), now using rollup-plugin-postcss instead
+  ],
   build: {
     copyPublicDir: false,
     lib: {
@@ -19,6 +23,14 @@ export default defineConfig({
         assetFileNames: "assets/[name][extname]",
         entryFileNames: "index.js",
       },
+      plugins: [
+        // Configure postcss to inject CSS into the bundle:
+        postcss({
+          inject: true,   // Inject CSS into JavaScript
+          extract: false, // Do not output a separate CSS file
+          minimize: true, // Optionally minimize the CSS
+        }),
+      ],
     },
     minify: true,
   },
@@ -27,4 +39,4 @@ export default defineConfig({
       "@": resolve(__dirname, "lib"),
     },
   },
-})
+});
