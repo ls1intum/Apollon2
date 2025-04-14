546e370868e587ad778dc1a1e559cb25f5d8352f
Belemir Kürün <kurunbelemir@gmail.com>
2025-04-14 16:25:55 +0200

    refactor: add localhost for allowing cors

--- standalone/server/src/middleware.ts
+++ standalone/server/src/middleware.ts
@@ -4,7 +4,10 @@ import express, { Express } from "express"
 export function configureMiddleware(app: Express) {
   app.use(
     cors({
-      origin: [process.env.FRONTEND_URL || "http://localhost:5173","http://localhost"],
+      origin: [
+        process.env.FRONTEND_URL || "http://localhost:5173",
+        "http://localhost",
+      ],
       methods: ["GET", "POST", "PUT", "DELETE"],
       allowedHeaders: ["Content-Type"],
     })
