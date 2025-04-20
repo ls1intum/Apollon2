f4512adc18aacbd792bd57a62979c64dac94c8ac
egenerse <egenerse@gmail.com>
2025-04-20 12:44:35 +0200

    fix: navigate caddy ws to path with ws

--- standalone/webapp/src/constants/urls.ts
+++ standalone/webapp/src/constants/urls.ts
@@ -1,4 +1,4 @@
 export const backendURL =
   import.meta.env.VITE_BACKEND_URL || "http://localhost:8000"
 export const backendWSSUrl =
-  import.meta.env.VITE_BACKEND_URL_WSS || "ws://localhost:4444"
+  import.meta.env.VITE_BACKEND_URL_WSS || "ws://localhost:4444/ws"
