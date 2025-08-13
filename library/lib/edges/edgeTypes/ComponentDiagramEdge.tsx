d1657fa1f44994b19f6bb0eec23b31af0cc12ec4
Belemir Kürün <kurunbelemir@gmail.com>
2025-08-13 18:58:39 +0200

    feat:add component edge

---
+++ library/lib/edges/edgeTypes/ComponentDiagramEdge.tsx
@@ -0,0 +1,49 @@
+// ComponentDiagramEdge.tsx
+import { StepPathEdge } from "../pathTypes/StepPathEdge"
+import { BaseEdgeProps } from "../GenericEdge"
+
+interface ComponentDiagramEdgeProps extends BaseEdgeProps {
+  allowMidpointDragging?: boolean
+  enableReconnection?: boolean
+}
+
+export const ComponentDiagramEdge = ({
+  id,
+  type,
+  source,
+  target,
+  sourceX,
+  sourceY,
+  targetX,
+  targetY,
+  sourcePosition,
+  targetPosition,
+  sourceHandleId,
+  targetHandleId,
+  data,
+  allowMidpointDragging = true,
+  enableReconnection = true,
+}: ComponentDiagramEdgeProps) => {
+  
+  return (
+    <StepPathEdge
+      id={id}
+      type={type}
+      source={source}
+      target={target}
+      sourceX={sourceX}
+      sourceY={sourceY}
+      targetX={targetX}
+      targetY={targetY}
+      sourcePosition={sourcePosition}
+      targetPosition={targetPosition}
+      sourceHandleId={sourceHandleId}
+      targetHandleId={targetHandleId}
+      data={data}
+      allowMidpointDragging={allowMidpointDragging}
+      enableReconnection={enableReconnection}
+    >
+  
+    </StepPathEdge>
+  )
+}
