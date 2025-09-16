b2c3a4d5f2871dc641892d74da18446e2b56092c
Belemir Kürün <kurunbelemir@gmail.com>
2025-09-16 15:30:14 +0300

    fix: update cors policy

--- standalone/server/src/middlewares/cors.ts
+++ standalone/server/src/middlewares/cors.ts
@@ -2,7 +2,7 @@ import cors from "cors"
 
 export function configureCors() {
   return cors({
-    origin: [process.env.FRONTEND_URL || "http://localhost:5173"],
+    origin: [process.env.FRONTEND_URL || "http://localhost:5173",  "http://localhost"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
