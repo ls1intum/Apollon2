74b416e1a529b71c5bc27d2ae9f54397eb757225
Belemir Kürün <kurunbelemir@gmail.com>
2025-09-16 15:33:11 +0300

    refactor: update format

--- standalone/server/src/middlewares/cors.ts
+++ standalone/server/src/middlewares/cors.ts
@@ -2,7 +2,10 @@ import cors from "cors"
 
 export function configureCors() {
   return cors({
-    origin: [process.env.FRONTEND_URL || "http://localhost:5173",  "http://localhost"],
+    origin: [
+      process.env.FRONTEND_URL || "http://localhost:5173",
+      "http://localhost",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
